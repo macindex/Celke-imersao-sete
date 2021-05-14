dd6ff480d728f74b38e9bc014963f0059a5d5218
marcio lira <machollanda@gmail.com>
2021-05-13 21:33:32 -0300

    add sequelize and database

--- semana-imersao-sete/api/app.js
+++ semana-imersao-sete/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+constdb= require("./models/db");
+
 app.get('/', function (req, res){
 	res.send('world');
 });
 
 app.listen(3000, function(){
-    console.log("Servidor iniciado na porta 3333: http://localhost:3333/");
+    console.log("Servidor iniciado na porta 3333: http://localhost:3000/");
 }); 
  
