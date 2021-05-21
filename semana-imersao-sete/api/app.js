bbbfb7e5d56c9c58470a5c4acb645b9e2fd89a86
marcio lira <machollanda@gmail.com>
2021-05-20 23:57:53 -0300

    create routes delete and cors implemented

--- semana-imersao-sete/api/app.js
+++ semana-imersao-sete/api/app.js
@@ -1,21 +1,60 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+
+    
+
+    app.use(cors());
+    next();
+});
+
+
 
 const Anuncio = require('./models/Anuncio');
 
 // const db= require("./models/db");
 
-app.get('/', function (req, res){
+app.get('/', async (req, res) => {
 	// res.send('Home page');
-    Anuncio.findAll({ order: [['id', 'ASC']] }).then(function(anuncios){
+    await Anuncio.findAll({ order: [['id', 'ASC']] }).then(function(anuncios){
         res.json({ anuncios });
     });
 });
 
-app.get('/visualizar', async (req, res) => {
-    res.send("visualizar")
+app.get('/visualizar/:id', async (req, res) => {
+    // res.send("ID: " + req.params.id);
+    await Anuncio.findByPk(req.params.id)
+    .then(anuncioBd => {
+        return res.json({
+            error: false,
+            anuncio: anuncioBd
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro: Anúncio nao encontrado!"
+        });
+    });
+});
+
+app.put('/editar', async (req, res) => {
+    await Anuncio.update(req.body, {
+        where: { id: req.body.id }
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Anúncio editado com sucesso!"
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro: Anúncio não editado com sucesso!"
+        });
+    });
 });
 
 app.post('/cadastrar', async(req, res) => {
@@ -33,7 +72,24 @@ app.post('/cadastrar', async(req, res) => {
             error: true,
             message: "Erro: Erro: Anúncio não cadastrado com sucesso!"
         });
-    })
+    });
+});
+
+app.delete('/apagar/:id', async (req, res) => {
+    await Anuncio.destroy({
+        where: { id: req.params.id }
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Anuncio apagado com sucesso!"
+        });
+    }).catch(function(err){
+        res.send('Erro: Anúncio não cadastrado com sucesso!');
+        return res.status(400).json({
+            error: true,
+            message: "Erro: Erro: Anúncio não apagado com sucesso!"
+        });
+    });
 });
 
 app.listen(3000, function(){
